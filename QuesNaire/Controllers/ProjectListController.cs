0e96cf710a640e8215f2902acb388240f5716fbf
zqa2012 <zqa778@gmail.com>
2019-12-24 23:24:52 +0800

    发布页更改

--- QuesNaire/Controllers/ProjectListController.cs
+++ QuesNaire/Controllers/ProjectListController.cs
@@ -3,8 +3,12 @@ using QuesNaire.App_Code;
 using QuesNaire.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -28,8 +32,11 @@ namespace QuesNaire.Controllers
                 Response.Cookies.Add(cookie);
             }
             else
+            {
+                Response.Redirect("~/Login/Index");
                 return View();
-            if (Request.Cookies["user_id"] != null)
+            }
+            if (id != null)
             {
                 NaireWebDataContext db = new NaireWebDataContext();
                 var rs = from r in db.user_info
@@ -38,15 +45,18 @@ namespace QuesNaire.Controllers
                          {
                              r.account,
                              r.name,
+                             r.password,
                              r.avatar
                          };
                 var name = rs.FirstOrDefault().name;
                 var account = rs.FirstOrDefault().account;
                 var avatar = rs.FirstOrDefault().avatar;
+                var password = rs.FirstOrDefault().password;
                 user.Name = name;
                 user.Id = int.Parse(id);
                 user.Account = account;
                 user.Avatar = avatar;
+                user.Password = password;
                 HttpCookie cookie = new HttpCookie("user_account");
                 cookie.Value = user.Account;
                 Response.Cookies.Add(cookie);
@@ -56,43 +66,72 @@ namespace QuesNaire.Controllers
                 HttpCookie cookie3 = new HttpCookie("user_avatar");
                 cookie3.Value = user.Avatar;
                 Response.Cookies.Add(cookie3);
+                HttpCookie cookie4 = new HttpCookie("user_password");
+                cookie4.Value = user.Password;
+                Response.Cookies.Add(cookie4);
+            }
+            else
+            {
+                Response.Redirect("~/Login/Index");
             }
             return View();
         }
         private UserInfo user = new UserInfo();
+        [HttpPost]
         public string upImg(string avatar,string id)
         {
 
-            //NaireWebDataContext db = new NaireWebDataContext();
-            //var user = db.user_info.Where(r => r.id.ToString() == id).FirstOrDefault();
-            //user.avatar = avatar;
-            //db.SubmitChanges();
+            NaireWebDataContext db = new NaireWebDataContext();
+            var user = db.user_info.Where(r => r.id.ToString() == id).FirstOrDefault();
+            user.avatar = "http://test.xkspbz.com/avatar/img"+GetImage(avatar, id);
+            db.SubmitChanges();
+            HttpCookie cookie3 = new HttpCookie("user_avatar");
+            cookie3.Value = "http://test.xkspbz.com/avatar/img" + GetImage(avatar, id);
+            Response.Cookies.Add(cookie3);
 
-            return PostResponse("http://test.xkspbz.com/avatar/getImgUrl.php", "img=" + avatar, out string statusCode);
+            return "1";
         }
-        public string PostResponse(string url, string postData, out string statusCode)
+        private string GetImage(string imgbyte,string id)
         {
-            string result = string.Empty;
-            //设置Http的正文
-            HttpContent httpContent = new StringContent(postData);
-            //设置Http的内容标头
-            httpContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            //设置Http的内容标头的字符
-            httpContent.Headers.ContentType.CharSet = "utf-8";
-            using (HttpClient httpClient = new HttpClient())
+            string result3 = string.Empty;
+            try
             {
-                //异步Post
-                HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
-                //输出Http响应状态码
-                statusCode = response.StatusCode.ToString();
-                //确保Http响应成功
-                if (response.IsSuccessStatusCode)
-                {
-                    //异步读取json
-                    result = response.Content.ReadAsStringAsync().Result;
-                }
+                string requestUri = "http://test.xkspbz.com/avatar/getImgUrl.php";//请求的url
+                HttpClient httpClient = new HttpClient();
+                //参数实例 p1=v1&p2=v2
+                string str = "img=" + imgbyte+"&id="+id;
+                var content = new StringContent(str, Encoding.UTF8, "application/x-www-form-urlencoded");
+                HttpResponseMessage result = httpClient.PostAsync(requestUri, content).Result;
+                string result2 = result.Content.ReadAsStringAsync().Result;
+                result3 = result2;
             }
-            return result;
+            catch (Exception e)
+            {
+                result3 = "error";
+            }
+            return result3;
+        } 
+        [HttpPost]
+        public string upInfo(string id, string name , string password)
+        {
+            NaireWebDataContext db = new NaireWebDataContext();
+            var rs = from r in db.user_info
+                     where r.id.ToString() == id
+                     select r;
+            if(rs.FirstOrDefault()!=null)
+            {
+                rs.FirstOrDefault().name = name;
+                rs.FirstOrDefault().password = password;
+            }
+            db.SubmitChanges();
+            HttpCookie cookie2 = new HttpCookie("user_name");
+            cookie2.Value = name;
+            Response.Cookies.Add(cookie2);
+            HttpCookie cookie4 = new HttpCookie("user_password");
+            cookie4.Value = password;
+            Response.Cookies.Add(cookie4);
+
+            return "1";
         }
     }
 }

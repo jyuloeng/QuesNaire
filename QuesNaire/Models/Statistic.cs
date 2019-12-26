aacbadebe6cc7271956fcdbce4b5ca63283c7eb7
zqa2012 <zqa778@gmail.com>
2019-12-27 01:35:38 +0800

    数据json可以查出来了

---
+++ QuesNaire/Models/Statistic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuesNaire.Models
+{
+    public class Statistic
+    {
+        public string title { get; set; }
+        public string update_time { get; set; }
+        public int data { get; set; }
+        public List<questions> questions = new List<questions> { };
+    }
+
+    public class questions
+    {
+        public string title { get; set; }
+        public int flag { get; set; }
+        public List<string> options = new List<string> { };
+        public List<string> replys = new List<string> { };
+    }
+}
